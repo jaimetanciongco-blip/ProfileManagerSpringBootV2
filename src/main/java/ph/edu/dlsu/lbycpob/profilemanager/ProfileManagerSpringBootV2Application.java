6540749ff5d73f4ba4bd0ef939d142ffb975e777
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-07-23 09:05:46 +0800

    test run

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/ProfileManagerSpringBootV2Application.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/ProfileManagerSpringBootV2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfileManagerSpringBootV2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(ProfileManagerSpringBootV2Application.class, args);
     }
-
 }
