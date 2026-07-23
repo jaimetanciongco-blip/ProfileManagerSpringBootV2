fd75d06cfcc9c0fabd33381b1e6df4c14b390dd4
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-07-23 08:31:32 +0800

    added project classes

---
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/ProfileManagerSpringBootV2Application.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.profilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagerSpringBootV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileManagerSpringBootV2Application.class, args);
+    }
+
+}
---
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/controller/PageController.java
@@ -0,0 +1,4 @@
+package ph.edu.dlsu.lbycpob.profilemanager.controller;
+
+public class PageController {
+}
