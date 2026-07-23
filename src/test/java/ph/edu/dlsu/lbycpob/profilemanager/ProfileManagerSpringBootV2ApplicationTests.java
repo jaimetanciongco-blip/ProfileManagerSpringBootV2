81f4dab9924803249f3a921c30cc4fba5c18b8a6
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-07-23 08:30:24 +0800

    Initial commit

---
+++ src/test/java/ph/edu/dlsu/lbycpob/profilemanager/ProfileManagerSpringBootV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.profilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagerSpringBootV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
