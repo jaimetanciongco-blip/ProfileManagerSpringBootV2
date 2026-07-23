4f5beaac67ef68032395f00108afb1a4a206c1c3
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-07-23 09:05:05 +0800

    added code in profile repository

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/repository/ProfileRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/repository/ProfileRepository.java
@@ -1,4 +1,17 @@
 package ph.edu.dlsu.lbycpob.profilemanager.repository;
 
-public interface ProfileRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+
+    List<Profile> findAllByOrderByNameAsc();
+
+    Optional<Profile> findByNameIgnoreCase(String name);
+
+    List<Profile> findByNameContainingIgnoreCaseOrderByNameAsc(String query);
 }
